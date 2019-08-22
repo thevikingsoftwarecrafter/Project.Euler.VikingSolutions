2b0d898f5ea0a61802fdbeb995525b761f8269e5
The Viking Software Crafter <sergi@thevikingsoftwarecrafter.com>
2019-08-22 16:14:25 +0200

    refactor: solving with tail recursion

--- src/Project.Euler.VikingSolutions/src/p001_MultiplesOf3And5/MultiplesOf3And5.cs
+++ src/Project.Euler.VikingSolutions/src/p001_MultiplesOf3And5/MultiplesOf3And5.cs
@@ -3,10 +3,25 @@ using System;
 namespace src.p001_MultiplesOf3And5 {
     public class MultiplesOf3And5 {
         public int GetSumBelow (int edge) {
-            if (edge >= 7) return 14;
-            if (edge >= 6) return 8;
-            if (edge >= 4) return 3;
-            return 0;
+            if (edge == 0) return 0;
+            int valueToCheck = (edge - 1);
+            
+            return FilteredValue(valueToCheck) + GetSumBelow (valueToCheck);
+        }
+
+        private int FilteredValue(int value)
+        {
+            return (IsMultipleOf3(value) || IsMultipleOf5(value)) ? value : 0;
+        }
+
+        private static bool IsMultipleOf5(int value)
+        {
+            return value % 5 == 0;
+        }
+
+        private static bool IsMultipleOf3(int value)
+        {
+            return value % 3 == 0;
         }
     }
 }
