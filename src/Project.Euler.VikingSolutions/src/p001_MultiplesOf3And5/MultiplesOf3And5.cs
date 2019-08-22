dfdb324c3691fcaf1b162c32f81a22bc412e3ff8
The Viking Software Crafter <sergi@thevikingsoftwarecrafter.com>
2019-08-22 16:01:27 +0200

    feat: solve basic cases unti 7, faking it

--- src/Project.Euler.VikingSolutions/src/p001_MultiplesOf3And5/MultiplesOf3And5.cs
+++ src/Project.Euler.VikingSolutions/src/p001_MultiplesOf3And5/MultiplesOf3And5.cs
@@ -2,8 +2,11 @@ using System;
 
 namespace src.p001_MultiplesOf3And5 {
     public class MultiplesOf3And5 {
-        public int[] GetAllBelow (int edge) {
-            return new int[] { 3 };
+        public int GetSumBelow (int edge) {
+            if (edge >= 7) return 14;
+            if (edge >= 6) return 8;
+            if (edge >= 4) return 3;
+            return 0;
         }
     }
 }
--- src/Project.Euler.VikingSolutions/test/p001_MultiplesOf3And5/MultiplesOf3And5Should.cs
+++ src/Project.Euler.VikingSolutions/test/p001_MultiplesOf3And5/MultiplesOf3And5Should.cs
@@ -1,19 +1,26 @@
 using System;
+using System.Collections.Generic;
 using src.p001_MultiplesOf3And5;
 using Xunit;
 
 namespace test.p001_MultiplesOf3And5 {
     public class MultiplesOf3And5Should {
-        [Fact]
-        public void GetAllMultiplesBelowEdge () {
+
+        [Theory]
+        [InlineData (3, 0)]
+        [InlineData (4, 3)]
+        [InlineData (5, 3)]
+        [InlineData (6, 8)]
+        [InlineData (7, 14)]
+        public void GetAllMultiplesBelowEdge (int edge, int expected) {
             //Arrange
             MultiplesOf3And5 multiplesOf3And5 = new MultiplesOf3And5 ();
 
             //Act
-            int[] actual = multiplesOf3And5.GetAllBelow (4);
+            int actual = multiplesOf3And5.GetSumBelow (edge);
 
             //Assert
-            Assert.Equal (new [] { 3 }, actual);
+            Assert.Equal (expected, actual);
         }
     }
 }
